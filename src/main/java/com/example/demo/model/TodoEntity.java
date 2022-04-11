8eca56770b285abb712b8fd4d353ae9e6dce353d
epson220 <62466439+epson220@users.noreply.github.com>
2022-04-11 16:57:36 +0900

    todo_entity

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,5 +1,17 @@
 package com.example.demo.model;
 
-public class TodoEntity {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoEntity {
+    private String id;
+    private String user_id;
+    private String title;
+    private boolean done;
 }
