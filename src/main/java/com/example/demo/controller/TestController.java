fe8f6af737d0122974e6e24be0ec959d5527eaf9
epson220 <62466439+epson220@users.noreply.github.com>
2022-04-14 01:26:28 +0900

    service

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -23,5 +25,26 @@ public class TestController {
     @GetMapping("/testRequestParam")
     public String testControllerRequestParam(@RequestParam(required = false) int id) {
         return "hello world id " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "hello world" + testRequestBodyDTO.getId() + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello world i m responseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("hello 400");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.badRequest().body(response);
     }
 }
