ce5d70c3dedaa350d8fd231d5de90d493aa258f7
epson220 <62466439+epson220@users.noreply.github.com>
2022-04-13 20:11:02 +0900

    TestRequestBodyDTO

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("test")
@@ -10,5 +13,15 @@ public class TestController {
     @GetMapping("/testGetMapping")
     public String testController() {
         return "Hello World!";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+        return "Hello world " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "hello world id " + id;
     }
 }
