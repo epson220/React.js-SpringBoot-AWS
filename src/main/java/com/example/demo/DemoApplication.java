5a1ba64a5ecd58092cea549d7508f432cd1fa160
epson220 <62466439+epson220@users.noreply.github.com>
2022-05-18 23:29:12 +0900

    create todo 서비스레이어 구현

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	String a = "dfasdfasd";
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,9 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -19,6 +23,25 @@ public class TodoService {
         // TodoEntity 검색
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        // 유효성 검증
+        if(entity == null) {
+            log.warn("entity 가 비어있음");
+            throw new RuntimeException("entity가 비어있음");
+        }
+
+        if(entity.getUserId() == null) {
+            log.warn("알려지지 않은 유저");
+            throw new RuntimeException("알려지지 않은 유저");
+        }
+
+        repository.save(entity);
+
+        log.info("Entity id : {} is saved", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
     }
 }
 
